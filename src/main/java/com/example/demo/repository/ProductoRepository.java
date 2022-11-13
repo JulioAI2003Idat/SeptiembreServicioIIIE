98fa00a8f4a159b7a02332f406607d3328f0facf
JulioAI2003Idat <julioe.a.i.2003@gmail>
2022-11-12 21:10:01 -0500

    usando usuario de bd

--- src/main/java/com/example/demo/repository/ProductoRepository.java
+++ src/main/java/com/example/demo/repository/ProductoRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.model.Producto;
 
+
 public interface ProductoRepository extends JpaRepository<Producto, Integer> {
 	
 
